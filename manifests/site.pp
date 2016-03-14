82fcbfaef0344c5f5fb006e4280e8a28d025e5f0
root <root@puppet.localdomain>
2016-03-14 20:03:30 +0000

    partie 1

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,88 +1,39 @@
-node /^puppet/ {
-  notify { 'hello': }
+#
+# OVH Breizhcamp
+#
+# @author: Arnaud Morin <arnaud.morin@corp.ovh.com>
+#
 
+#
+# Puppet Master node definition
+#
+node /^puppet/ {
+  # Hello world!
+  notify { 'Bonjour tout le monde du breizhcamp !!!': }
+
+  # Install puppetdb
   class {'::puppetdb':
     listen_address => $::ipaddress,
   }
   class { 'puppetdb::master::config': }
 
-
-  class { 'haproxy': }
-
-  haproxy::listen { 'stats':
-    ipaddress        => $::ipaddress,
-    ports            => '9090',
-    options          => {
-      'mode'  => 'http',
-      'stats' => [
-        'uri /',
-        'auth puppet:puppet'
-      ],
-    },
-  }
-
-  haproxy::listen { 'web':
-    ipaddress        => $::ipaddress,
-    ports            => '80',
-    options          => {
-      'option'  => ['tcplog'],
-      'balance' => 'roundrobin',
-    },
-  }
-
+  # Make sure puppet agent run every minute
   cron { 'puppet-agent':
     ensure      => present,
     user        => root,
     minute      => '*',
     command     => '/opt/puppetlabs/bin/puppet agent -t -v',
   }
+
+  # TODO: install and configure haproxy
 }
 
 
+
+#
+# Web Server node definition
+#
 node /^web/ {
-  class { 'python' :
-    version    => 'system',
-    pip        => 'present',
-    dev        => 'absent',
-    virtualenv => 'absent',
-    gunicorn   => 'absent',
-  }
-
-  python::pip { 'Flask': }
-#  python::pip { 'Flask_redis': }
-
-#  package { 'redis-server':
-#    ensure => latest,
-#  }
-
-  @@haproxy::balancermember { $::fqdn:
-    listening_service => 'web',
-    ipaddresses       => $::ipaddress,
-    server_names      => $::hostname,
-    ports             => '5000',
-    options           => 'check',
-  }
-
-  cron { 'puppet-agent':
-    ensure      => present,
-    user        => root,
-    minute      => '*',
-    command     => '/opt/puppetlabs/bin/puppet agent -t -v',
-  }
-
-  package { 'git':
-    ensure => 'latest',
-  }
-  ->
-  vcsrepo { '/opt/demoflask':
-    ensure => latest,
-    provider => git,
-    source => 'https://github.com/arnaudmorin/puppet-demoflask.git',
-  }
-  ->
-  exec { '/opt/demoflask/start.py &':
-    unless      => '/bin/pidof -x start.py',
-    require     => Python::Pip['Flask'],
-  }
+  # TODO: install python web server
+  # TODO: declare haproxy backend
 }
-
